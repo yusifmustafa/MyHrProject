c054993ae6ec8d58a81a176ebad8bd40c705ec7b
Yusif Mustafa <109087614+yusifmustafa@users.noreply.github.com>
2022-11-06 22:35:48 +0400

    all ok.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Adduser from "./components/Adduser";
 import Tablelist from "./components/Tablelist";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
 
       <>
         <Route path="/tablelist" element={<Tablelist />} />
